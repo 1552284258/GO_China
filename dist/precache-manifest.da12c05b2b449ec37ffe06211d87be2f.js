d9bdcb23d90b632f60a82bd59bc4ad68d114671d
1552284258 <1552284258@qq.com>
2020-02-11 17:23:19 +0800

    中国加油

---
+++ dist/precache-manifest.da12c05b2b449ec37ffe06211d87be2f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8fe6b1912b811854a6",
+    "url": "css/app.310846be.css"
+  },
+  {
+    "revision": "e40c406558aa2feb09dd",
+    "url": "css/chunk-vendors.d39e9fad.css"
+  },
+  {
+    "revision": "b05b067c41a8ee370eb4",
+    "url": "css/hotsearch.3669d3e3.css"
+  },
+  {
+    "revision": "baf723f57318a6d5932d",
+    "url": "css/index.86c7f14b.css"
+  },
+  {
+    "revision": "241cf800b1796a515d3c",
+    "url": "css/news.1e29ddc5.css"
+  },
+  {
+    "revision": "c7e192985233cdfe8e15",
+    "url": "css/qxmap.e3036869.css"
+  },
+  {
+    "revision": "26fa833a4e715b2b0408",
+    "url": "css/yqdt.12bd989f.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "5d5571ac61c694dc769bc33923fe2b30",
+    "url": "index.html"
+  },
+  {
+    "revision": "ed8fe6b1912b811854a6",
+    "url": "js/app.bedf5a52.js"
+  },
+  {
+    "revision": "e40c406558aa2feb09dd",
+    "url": "js/chunk-vendors.0bb0b80f.js"
+  },
+  {
+    "revision": "b05b067c41a8ee370eb4",
+    "url": "js/hotsearch.c8506793.js"
+  },
+  {
+    "revision": "0559f2f21b77fb795392",
+    "url": "js/hotsearch~qxmap.550d1178.js"
+  },
+  {
+    "revision": "baf723f57318a6d5932d",
+    "url": "js/index.a70de3b7.js"
+  },
+  {
+    "revision": "241cf800b1796a515d3c",
+    "url": "js/news.2796c556.js"
+  },
+  {
+    "revision": "c7e192985233cdfe8e15",
+    "url": "js/qxmap.387db69f.js"
+  },
+  {
+    "revision": "26fa833a4e715b2b0408",
+    "url": "js/yqdt.d458431a.js"
+  },
+  {
+    "revision": "6f460eda32e1cd81faa8c4cd41b0d043",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
